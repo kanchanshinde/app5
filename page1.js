5dbacf53a6692f7b8af1bfa526ef5ccafa287b9f
kanchan <kanchanshinde27@gmail.com>
2019-05-29 16:51:25 +0530

    2file added

---
+++ page1.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const prod= require('./product');
+const userRouter=require('./user');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.use(prod);
+app.use(userRouter);
+
+
+app.listen(3000, '0.0.0.0', () => {
+    console.log(`Server started on port 3000`);
+});
+
+
+
+
+
+
